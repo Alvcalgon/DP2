9ca9e3a3da98b4d442556dc99bc5df35195d20bb
antnolang <antnolangos@gmail.com>
2019-02-23 09:48:25 +0100

    Añadida restricción Unique en Enrolment

--- Acme-Madruga/src/main/java/domain/Enrolment.java
+++ Acme-Madruga/src/main/java/domain/Enrolment.java
@@ -7,8 +7,10 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -17,6 +19,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(uniqueConstraints = {
+	@UniqueConstraint(columnNames = {
+		"brotherhood", "member"
+	})
+})
 public class Enrolment extends DomainEntity {
 
 	// Constructor
