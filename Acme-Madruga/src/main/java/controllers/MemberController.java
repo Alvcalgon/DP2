d5046541ca408bde3331dd12c52e3b95a9f8c1c6
marjimveg1 <maria-2696@hotmail.com>
2019-02-24 17:35:30 +0100

    Update MemberController.java

--- Acme-Madruga/src/main/java/controllers/MemberController.java
+++ Acme-Madruga/src/main/java/controllers/MemberController.java
@@ -1,11 +1,18 @@
 
 package controllers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import services.MemberService;
+
+import com.mysql.fabric.xmlrpc.base.Member;
 
 @Controller
 @RequestMapping(value = "/member")
@@ -25,18 +32,18 @@ public class MemberController extends AbstractController {
 
 	// List
 
-	//	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	//	public ModelAndView list(@RequestParam final int brotherhoodId) {
-	//		ModelAndView result;
-	//		Collection<Member> members;
-	//
-	//		members = this.memberService.findEnroledMemberByProcession(brotherhoodId);
-	//
-	//		result = new ModelAndView("actor/list");
-	//
-	//		result.addObject("actors", members);
-	//		result.addObject("requestURI", "member/list.do");
-	//		return result;
-	//	}
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list(@RequestParam final int brotherhoodId) {
+		ModelAndView result;
+		Collection<Member> members;
+
+		members = this.memberService.findEnroledMemberByProcession(brotherhoodId);
+
+		result = new ModelAndView("actor/list");
+
+		result.addObject("actors", members);
+		result.addObject("requestURI", "member/list.do");
+		return result;
+	}
 
 }
