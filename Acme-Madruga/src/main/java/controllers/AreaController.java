209a57af14edcbca61bc7cad159dbe4fafd8a07a
marjimveg1 <maria-2696@hotmail.com>
2019-02-25 11:55:04 +0100

    area

--- Acme-Madruga/src/main/java/controllers/AreaController.java
+++ Acme-Madruga/src/main/java/controllers/AreaController.java
@@ -1,12 +1,29 @@
 
 package controllers;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.AreaService;
+import services.UtilityService;
+import domain.Area;
 
 @Controller
 @RequestMapping(value = "/area")
 public class AreaController extends AbstractController {
+
+	@Autowired
+	private AreaService		areaService;
+
+	@Autowired
+	private UtilityService	utilityService;
+
 
 	// Constructors -----------------------------------------------------------
 
@@ -16,4 +33,24 @@ public class AreaController extends AbstractController {
 
 	// Float display ---------------------------------------------------------------		
 
+	@RequestMapping(value = "/display", method = RequestMethod.GET)
+	public ModelAndView display(@RequestParam final int areaId) {
+		ModelAndView result;
+		Area area;
+		Collection<String> pictures;
+
+		result = new ModelAndView("area/display");
+
+		try {
+			area = this.areaService.findOne(areaId);
+			pictures = this.utilityService.getSplittedString(area.getPictures());
+
+			result.addObject("area", area);
+			result.addObject("pictures", pictures);
+		} catch (final Exception e) {
+			result = new ModelAndView("redirect:../error.do");
+		}
+
+		return result;
+	}
 }
