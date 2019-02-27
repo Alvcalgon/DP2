3c0e810d06b747f26be22b00b7e0a4afec6ac02f
antnolang <antnolangos@gmail.com>
2019-02-27 12:57:24 +0100

    Limpiando código de contoladores Antonio

--- Acme-Madruga/src/main/java/controllers/member/FinderMemberController.java
+++ Acme-Madruga/src/main/java/controllers/member/FinderMemberController.java
@@ -1,12 +1,3 @@
-/*
- * CustomerController.java
- * 
- * Copyright (C) 2018 Universidad de Sevilla
- * 
- * The use of this project is hereby constrained to the conditions of the
- * TDG Licence, a copy of which you may download from
- * http://www.tdg-seville.info/License.html
- */
 
 package controllers.member;
 
