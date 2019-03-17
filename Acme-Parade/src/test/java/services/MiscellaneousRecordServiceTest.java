476daeeae790182081233c670b428ba89eeeb4c1
marjimveg1 <maria-2696@hotmail.com>
2019-03-17 20:38:19 +0100

    test maria

---
+++ Acme-Parade/src/test/java/services/MiscellaneousRecordServiceTest.java
@@ -0,0 +1,185 @@
+
+package services;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.MiscellaneousRecord;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class MiscellaneousRecordServiceTest extends AbstractTest {
+
+	// Services under test --------------------------
+
+	@Autowired
+	private MiscellaneousRecordService	miscellaneousRecordService;
+
+
+	// Other supporting services --------------------
+
+	// Suite test -----------------------------------
+
+	@Test
+	public void driverCreate() {
+		final Object testingData[][] = {
+			/*
+			 * A: Req1, 3.1
+			 * B: Crea adecuadamente un MiscellaneousRecord
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title MiscellaneousRecord test", "text MiscellaneousRecord test", null
+			},
+
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un MiscellaneousRecord con title en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", " ", "text MiscellaneousRecord test", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un MiscellaneousRecord con con text en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title MiscellaneousRecord test", "", ConstraintViolationException.class
+			},
+
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.templateCreate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
+
+	}
+
+	protected void templateCreate(final String username, final String title, final String text, final Class<?> expected) {
+		Class<?> caught;
+		final MiscellaneousRecord miscellaneousRecord;
+		final MiscellaneousRecord miscellaneousRecordSaved;
+
+		this.startTransaction();
+
+		caught = null;
+		try {
+			super.authenticate(username);
+
+			miscellaneousRecord = this.miscellaneousRecordService.create();
+
+			miscellaneousRecord.setText(text);
+			miscellaneousRecord.setTitle(title);
+
+			miscellaneousRecordSaved = this.miscellaneousRecordService.save(miscellaneousRecord);
+
+			this.miscellaneousRecordService.flush();
+
+			Assert.notNull(miscellaneousRecordSaved);
+
+			super.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.rollbackTransaction();
+
+		super.checkExceptions(expected, caught);
+	}
+
+	@Test
+	public void driverEdit() {
+		final Object testingData[][] = {
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar adecuadamente un MiscellaneousRecord
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "miscellaneousRecord1", "title MiscellaneousRecord test", "text MiscellaneousRecord test", null
+			},
+
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un MiscellaneousRecord con title en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "miscellaneousRecord1", " ", "text MiscellaneousRecord test", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un MiscellaneousRecord con con text en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "miscellaneousRecord1", "title MiscellaneousRecord test", "", ConstraintViolationException.class
+			},
+
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un MiscellaneousRecord que no es del principal
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "miscellaneousRecord3", "title MiscellaneousRecord test", "text MiscellaneousRecord test", IllegalArgumentException.class
+			},
+
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.templateEdit((String) testingData[i][0], super.getEntityId((String) testingData[i][1]), (String) testingData[i][2], (String) testingData[i][3], (Class<?>) testingData[i][4]);
+
+	}
+	protected void templateEdit(final String username, final Integer miscellaneousRecordId, final String title, final String text, final Class<?> expected) {
+		Class<?> caught;
+		MiscellaneousRecord miscellaneousRecord;
+		MiscellaneousRecord miscellaneousRecordSaved;
+
+		this.startTransaction();
+
+		caught = null;
+		try {
+			super.authenticate(username);
+
+			miscellaneousRecord = this.miscellaneousRecordService.findOneEdit(miscellaneousRecordId);
+
+			miscellaneousRecord.setText(text);
+			miscellaneousRecord.setTitle(title);
+
+			miscellaneousRecordSaved = this.miscellaneousRecordService.save(miscellaneousRecord);
+
+			this.miscellaneousRecordService.flush();
+
+			Assert.notNull(miscellaneousRecordSaved);
+
+			super.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.rollbackTransaction();
+
+		super.checkExceptions(expected, caught);
+	}
+
+}
---
+++ Acme-Parade/src/test/java/services/PeriodRecordServiceTest.java
@@ -0,0 +1,281 @@
+
+package services;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.PeriodRecord;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class PeriodRecordServiceTest extends AbstractTest {
+
+	// Services under test --------------------------
+
+	@Autowired
+	private PeriodRecordService	periodRecordService;
+
+
+	// Other supporting services --------------------
+
+	// Suite test -----------------------------------
+
+	@Test
+	public void driverCreate() {
+		final Object testingData[][] = {
+			/*
+			 * A: Req1, 3.1
+			 * B: Crea adecuadamente un periodRecord
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "text periodRecord test", 1966, 2000, "http://asdf.com", null
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear adecuadamente un periodRecord con startYear==1 y endYeard==1
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "text periodRecord test", 1966, 2000, "http://asdf.com", null
+			},
+
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un periodRecord con title en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "", "text periodRecord test", 1966, 2000, "http://asdf.com", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un periodRecord con con text en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "", 1966, 2000, "http://asdf.com", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un periodRecord con photo en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "text periodRecord test", 1966, 2000, "", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un periodRecord con photo invalida
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "text periodRecord test", 1966, 2000, "esto no es foto", IllegalArgumentException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un legalRecord startYear cero
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "text periodRecord test", 0, 2000, "http://asdf.com", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Crear un legalRecord endYear menor que startYear
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "title periodRecord test", "text periodRecord test", 1966, 1700, "http://asdf.com", IllegalArgumentException.class
+			},
+
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.templateCreate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Integer) testingData[i][3], (Integer) testingData[i][4], (String) testingData[i][5], (Class<?>) testingData[i][6]);
+
+	}
+	protected void templateCreate(final String username, final String title, final String text, final Integer startYear, final Integer endYear, final String photo, final Class<?> expected) {
+		Class<?> caught;
+		PeriodRecord periodRecord;
+		PeriodRecord periodRecordSaved;
+
+		this.startTransaction();
+
+		caught = null;
+		try {
+			super.authenticate(username);
+
+			periodRecord = this.periodRecordService.create();
+
+			periodRecord.setText(text);
+			periodRecord.setTitle(title);
+			periodRecord.setEndYear(endYear);
+			periodRecord.setPhotos(photo);
+			periodRecord.setStartYear(startYear);
+
+			periodRecordSaved = this.periodRecordService.save(periodRecord);
+
+			this.periodRecordService.flush();
+
+			Assert.notNull(periodRecordSaved);
+
+			super.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.rollbackTransaction();
+
+		super.checkExceptions(expected, caught);
+	}
+
+	@Test
+	public void driverEdit() {
+		final Object testingData[][] = {
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar adecuadamente un periodRecord
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "text periodRecord test", 1966, 2000, "http://asdf.com", null
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar adecuadamente un periodRecord con startYear==1 y endYeard==1
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "text periodRecord test", 1, 1, "http://asdf.com", null
+			},
+
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un periodRecord con title en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "", "text periodRecord test", 1966, 2000, "http://asdf.com", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un periodRecord con con text en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "", 1966, 2000, "http://asdf.com", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un periodRecord con photo en blanco
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "text periodRecord test", 1966, 2000, "", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un periodRecord con photo invalida
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "text periodRecord test", 1966, 2000, "esto no es foto", IllegalArgumentException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un legalRecord startYear negativo
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "text periodRecord test", -1, 2000, "http://asdf.com", ConstraintViolationException.class
+			},
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un legalRecord endYear menor que starYear
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord1", "title periodRecord test", "text periodRecord test", 2000, 21, "http://asdf.com", IllegalArgumentException.class
+			},
+
+			/*
+			 * A: Req1, 3.1
+			 * B: Editar un legalRecord que no es del principal
+			 * C:
+			 * D:
+			 */
+			{
+				"brotherhood1", "periodRecord3", "title periodRecord test", "text periodRecord test", 21, 21, "http://asdf.com", IllegalArgumentException.class
+			},
+
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.templateEdit((String) testingData[i][0], super.getEntityId((String) testingData[i][1]), (String) testingData[i][2], (String) testingData[i][3], (Integer) testingData[i][4], (Integer) testingData[i][5], (String) testingData[i][6],
+				(Class<?>) testingData[i][7]);
+
+	}
+	protected void templateEdit(final String username, final Integer periodRecordId, final String title, final String text, final Integer startYear, final Integer endYear, final String photo, final Class<?> expected) {
+		Class<?> caught;
+		PeriodRecord periodRecord;
+		PeriodRecord periodRecordSaved;
+
+		this.startTransaction();
+
+		caught = null;
+		try {
+			super.authenticate(username);
+
+			periodRecord = this.periodRecordService.findOne(periodRecordId);
+
+			periodRecord.setText(text);
+			periodRecord.setTitle(title);
+			periodRecord.setEndYear(endYear);
+			periodRecord.setPhotos(photo);
+			periodRecord.setStartYear(startYear);
+
+			periodRecordSaved = this.periodRecordService.save(periodRecord);
+
+			this.periodRecordService.flush();
+
+			Assert.notNull(periodRecordSaved);
+
+			super.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.rollbackTransaction();
+
+		super.checkExceptions(expected, caught);
+	}
+
+}
